64b7f85b734d9a20c01bcdbf303de725ef15ffd1
Thierno Amadou malady Diallo <thiernomalady.diallo@gmail.com>
2025-10-24 00:30:05 +0000

    vite 3

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: [
+      'projet-cancer-front.onrender.com', // 🔹 ton domaine Render
+      'localhost',
+    ],
   },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
   },
-  allowedHosts: [
-    'projet-cancer-front.onrender.com', // ✅ autorise ton domaine Render
-    'localhost', // pour les tests locaux
-  ],
 })
